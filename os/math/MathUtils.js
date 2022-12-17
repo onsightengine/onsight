b75a4adca1c93023b08923212effcf6ecb647038
Stephens Nunnally <stevinz@gmail.com>
2022-12-16 18:09:37 -0800

    Added math library

---
+++ os/math/MathUtils.js
@@ -0,0 +1,165 @@
+/** /////////////////////////////////////////////////////////////////////////////////
+//
+// @description Onsight Engine
+// @about       Powerful, easy-to-use JavaScript video game and application creation engine.
+// @author      Stephens Nunnally <@stevinz>
+// @license     MIT - Copyright (c) 2021-2022 Stephens Nunnally and Scidian Studios
+// @source      https://github.com/scidian/onsight-engine
+//
+///////////////////////////////////////////////////////////////////////////////////*/
+//
+//  Additional Source(s)
+//      MIT     https://github.com/mrdoob/three.js/blob/dev/src/math/MathUtils.js
+//
+/////////////////////////////////////////////////////////////////////////////////////
+//
+//  Angles
+//      radiansToDegrees        Converts radians to degrees
+//      degreesToRadians        Converts degrees to radians
+//  Common
+//      clamp                   Clamps a number between min and max
+//      damp                    Time based linear interpolation
+//      lerp                    Linear interpolation
+//      roundTo                 Returns a number rounded to 'decimalPlaces'
+//  Fuzzy Comparison
+//      fuzzyFloat              Compare two floats
+//      fuzzyVector             Compare two Vector3
+//      fuzzyQuaternion         Compare two Quaternion
+//  Geometry
+//      isPowerOfTwo            Checks if a number is power of 2
+//  Number Functions
+//      addCommas               Formats a number into a string with commas added for large numbers
+//      countDecimals           Counts significant decimal places
+//      isNumber                Checks if 'number' is a valid number
+//  Polygon
+//      lineCollision           Check if two lines are intersecting
+//      lineRectCollision       Checks if a line is intersecting a rectangle
+//  Random
+//      randomFloat             Random float from min (inclusive) to max (exclusive)
+//      randomInt               Random integer from min (inclusive) to max (exclusive)
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+class MathUtils {
+
+    //////////////////// Angles
+
+    static radiansToDegrees(radians) {
+        return radians * (180 / Math.PI);
+    }
+
+    static degreesToRadians(degrees) {
+        return (Math.PI / 180) * degrees;
+    }
+
+    //////////////////// Fuzzy Compare
+
+    /** Compares two floats to see if they're almost the same */
+    static fuzzyFloat(a, b, tolerance = 0.001) {
+        return ((a < (b + tolerance)) && (a > (b - tolerance)));
+    }
+
+    static fuzzyVector(a, b, tolerance = 0.001) {
+        if (MathUtils.fuzzyFloat(a.x, b.x, tolerance) === false) return false;
+        if (MathUtils.fuzzyFloat(a.y, b.y, tolerance) === false) return false;
+        if (MathUtils.fuzzyFloat(a.z, b.z, tolerance) === false) return false;
+        return true;
+    }
+
+    static fuzzyQuaternion(a, b, tolerance = 0.001) {
+        if (MathUtils.fuzzyVector(a, b, tolerance) === false) return false;
+        if (MathUtils.fuzzyFloat(a.w, b.w, tolerance) === false) return false;
+        return true;
+    }
+
+    //////////////////// Common
+
+    /** Clamps a number between min and max */
+    static clamp(number, min, max) {
+        return Math.min(Math.max(number, min), max);
+    }
+
+    /** http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/ */
+    static damp(x, y, lambda, dt) {
+        return MathUtils.lerp(x, y, 1 - Math.exp(- lambda * dt));
+    }
+
+    /** https://en.wikipedia.org/wiki/Linear_interpolation */
+    static lerp(x, y, t) {
+        return (1 - t) * x + t * y;
+    }
+
+    /** Returns a number rounded to 'decimalPlaces' */
+    static roundTo(number, decimalPlaces = 0) {
+        const shift = Math.pow(10, decimalPlaces);
+        return Math.round(number * shift) / shift;
+    }
+
+    ///// Geometry
+
+    /** Checks if a number is power of 2 */
+    static isPowerOfTwo(value) {
+        return (value & (value - 1)) === 0 && value !== 0;
+    }
+
+    //////////////////// Numbers
+
+    /** Formats a number into a string with commas added for large numbers (i.e. 12000 => 12,000) */
+    static addCommas(number) {
+        return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+    }
+
+    /** Counts significant decimal places */
+    static countDecimals(number) {
+        if (Math.floor(number.valueOf()) === number.valueOf()) return 0;
+        return number.toString().split('.')[1].length || 0;
+    }
+
+    /** Checks if 'number' is a valid number */
+    static isNumber(number) {
+        return (number !== undefined && number !== null && typeof number === 'number' && ! Number.isNaN(number) && Number.isFinite(number));
+    }
+
+    //////////////////// Polygon
+
+    /** Check if two lines are intersecting */
+    static lineCollision(x1, y1, x2, y2, x3, y3, x4, y4) {
+        // Calculate the direction of the lines
+        let denom = ((y4 - y3) * (x2 - x1)) - ((x4 - x3) * (y2 - y1));
+        if (MathUtils.fuzzyFloat(denom, 0, 0.0000001)) return false;
+        let ua = (((x4 - x3) * (y1 - y3)) - ((y4 - y3) * (x1 - x3))) / denom;
+        let ub = (((x2 - x1) * (y1 - y3)) - ((y2 - y1) * (x1 - x3))) / denom;
+
+        if ((ua >= 0) && (ua <= 1) && (ub >= 0) && (ub <= 1)) {
+            // If intersecting, the intersection points are
+            // 		crossX = x1 + (uA * (x2 - x1));
+            // 		crossY = y1 + (uA * (y2 - y1));
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks if a line is intersecting a rectangle */
+    static lineRectCollision(x1, y1, x2, y2, left, top, right, down) {
+        const rectLeft =    MathUtils.lineCollision(x1, y1, x2, y2, left, top, left, down);
+        const rectRight =   MathUtils.lineCollision(x1, y1, x2, y2, right, top, right, down);
+        const rectTop =     MathUtils.lineCollision(x1, y1, x2, y2, left, top, right, top);
+        const rectDown =    MathUtils.lineCollision(x1, y1, x2, y2, left, down, right, down);
+        return (rectLeft || rectRight || rectTop || rectDown);
+    }
+
+    //////////////////// Random
+
+    /** Random float from min (inclusive) to max (exclusive) */
+    static randomFloat(min, max) {
+        return min + Math.random() * (max - min);
+    }
+
+    /** Random integer from min (inclusive) to max (exclusive), randomInt(0, 3) => expected output: 0, 1 or 2 */
+    static randomInt(min = 0, max = 1) {
+        return min + Math.floor(Math.random() * (max - min));
+    }
+
+}
+
+export { MathUtils };
---
+++ os/math/Quaternion.js
@@ -0,0 +1,451 @@
+/** /////////////////////////////////////////////////////////////////////////////////
+//
+// @description Onsight Engine
+// @about       Powerful, easy-to-use JavaScript video game and application creation engine.
+// @author      Stephens Nunnally <@stevinz>
+// @license     MIT - Copyright (c) 2021-2022 Stephens Nunnally and Scidian Studios
+// @source      https://github.com/scidian/onsight-engine
+//
+///////////////////////////////////////////////////////////////////////////////////*/
+//
+//  Additional Source(s)
+//      MIT     https://github.com/mrdoob/three.js/blob/dev/src/math/Quaternion.js - Aug 4, 2022
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+class Quaternion {
+
+    constructor(x = 0, y = 0, z = 0, w = 1) {
+        this.isQuaternion = true;
+
+        this._x = x;
+        this._y = y;
+        this._z = z;
+        this._w = w;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(value) {
+        this._x = value;
+        this._onChangeCallback();
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set y(value) {
+        this._y = value;
+        this._onChangeCallback();
+    }
+
+    get z() {
+        return this._z;
+    }
+
+    set z(value) {
+        this._z = value;
+        this._onChangeCallback();
+    }
+
+    get w() {
+        return this._w;
+    }
+
+    set w(value) {
+        this._w = value;
+        this._onChangeCallback();
+    }
+
+    set(x, y, z, w) {
+        this._x = x;
+        this._y = y;
+        this._z = z;
+        this._w = w;
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    clone() {
+        return new this.constructor(this._x, this._y, this._z, this._w);
+    }
+
+    copy(quaternion) {
+        this._x = quaternion.x;
+        this._y = quaternion.y;
+        this._z = quaternion.z;
+        this._w = quaternion.w;
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    setFromEuler(euler, update) {
+        // http://www.mathworks.com/matlabcentral/fileexchange/20696-function-to-convert-between-dcm-euler-angles-quaternions-and-euler-vectors/content/SpinCalc.m
+
+        const x = euler._x, y = euler._y, z = euler._z, order = euler._order;
+
+        const cos = Math.cos;
+        const sin = Math.sin;
+
+        const c1 = cos(x / 2);
+        const c2 = cos(y / 2);
+        const c3 = cos(z / 2);
+
+        const s1 = sin(x / 2);
+        const s2 = sin(y / 2);
+        const s3 = sin(z / 2);
+
+        switch (order) {
+            case 'XYZ':
+                this._x = s1 * c2 * c3 + c1 * s2 * s3;
+                this._y = c1 * s2 * c3 - s1 * c2 * s3;
+                this._z = c1 * c2 * s3 + s1 * s2 * c3;
+                this._w = c1 * c2 * c3 - s1 * s2 * s3;
+                break;
+            case 'YXZ':
+                this._x = s1 * c2 * c3 + c1 * s2 * s3;
+                this._y = c1 * s2 * c3 - s1 * c2 * s3;
+                this._z = c1 * c2 * s3 - s1 * s2 * c3;
+                this._w = c1 * c2 * c3 + s1 * s2 * s3;
+                break;
+            case 'ZXY':
+                this._x = s1 * c2 * c3 - c1 * s2 * s3;
+                this._y = c1 * s2 * c3 + s1 * c2 * s3;
+                this._z = c1 * c2 * s3 + s1 * s2 * c3;
+                this._w = c1 * c2 * c3 - s1 * s2 * s3;
+                break;
+            case 'ZYX':
+                this._x = s1 * c2 * c3 - c1 * s2 * s3;
+                this._y = c1 * s2 * c3 + s1 * c2 * s3;
+                this._z = c1 * c2 * s3 - s1 * s2 * c3;
+                this._w = c1 * c2 * c3 + s1 * s2 * s3;
+                break;
+            case 'YZX':
+                this._x = s1 * c2 * c3 + c1 * s2 * s3;
+                this._y = c1 * s2 * c3 + s1 * c2 * s3;
+                this._z = c1 * c2 * s3 - s1 * s2 * c3;
+                this._w = c1 * c2 * c3 - s1 * s2 * s3;
+                break;
+            case 'XZY':
+                this._x = s1 * c2 * c3 - c1 * s2 * s3;
+                this._y = c1 * s2 * c3 - s1 * c2 * s3;
+                this._z = c1 * c2 * s3 + s1 * s2 * c3;
+                this._w = c1 * c2 * c3 + s1 * s2 * s3;
+                break;
+            default:
+                console.warn('ONE.Quaternion: .setFromEuler() encountered an unknown order: ' + order);
+        }
+
+        if (update !== false ) this._onChangeCallback();
+
+        return this;
+    }
+
+    setFromAxisAngle(axis, angle) {
+        // http://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToQuaternion/index.htm
+        // assumes axis is normalized
+
+        const halfAngle = angle / 2, s = Math.sin(halfAngle);
+
+        this._x = axis.x * s;
+        this._y = axis.y * s;
+        this._z = axis.z * s;
+        this._w = Math.cos(halfAngle);
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    setFromRotationMatrix(m) {
+        // http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+        // assumes the upper 3x3 of m is a pure rotation matrix (i.e, unscaled)
+
+        const te = m.elements,
+            m11 = te[0], m12 = te[4], m13 = te[8],
+            m21 = te[1], m22 = te[5], m23 = te[9],
+            m31 = te[2], m32 = te[6], m33 = te[10],
+            trace = m11 + m22 + m33;
+
+        if (trace > 0) {
+            const s = 0.5 / Math.sqrt( trace + 1.0 );
+            this._w = 0.25 / s;
+            this._x = (m32 - m23) * s;
+            this._y = (m13 - m31) * s;
+            this._z = (m21 - m12) * s;
+
+        } else if (m11 > m22 && m11 > m33) {
+            const s = 2.0 * Math.sqrt(1.0 + m11 - m22 - m33);
+            this._w = (m32 - m23) / s;
+            this._x = 0.25 * s;
+            this._y = (m12 + m21) / s;
+            this._z = (m13 + m31) / s;
+
+        } else if ( m22 > m33 ) {
+            const s = 2.0 * Math.sqrt(1.0 + m22 - m11 - m33);
+            this._w = (m13 - m31) / s;
+            this._x = (m12 + m21) / s;
+            this._y = 0.25 * s;
+            this._z = (m23 + m32) / s;
+
+        } else {
+            const s = 2.0 * Math.sqrt(1.0 + m33 - m11 - m22);
+            this._w = (m21 - m12) / s;
+            this._x = (m13 + m31) / s;
+            this._y = (m23 + m32) / s;
+            this._z = 0.25 * s;
+        }
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    setFromUnitVectors( vFrom, vTo ) {
+        // assumes direction vectors vFrom and vTo are normalized
+
+        let r = vFrom.dot(vTo) + 1;
+
+        if (r < Number.EPSILON) {
+            // vFrom and vTo point in opposite directions
+            r = 0;
+
+            if (Math.abs(vFrom.x) > Math.abs(vFrom.z)) {
+                this._x = -vFrom.y;
+                this._y = vFrom.x;
+                this._z = 0;
+                this._w = r;
+            } else {
+                this._x = 0;
+                this._y = -vFrom.z;
+                this._z = vFrom.y;
+                this._w = r;
+            }
+
+        } else {
+            this._x = vFrom.y * vTo.z - vFrom.z * vTo.y;
+            this._y = vFrom.z * vTo.x - vFrom.x * vTo.z;
+            this._z = vFrom.x * vTo.y - vFrom.y * vTo.x;
+            this._w = r;
+        }
+
+        return this.normalize();
+    }
+
+    angleTo(q) {
+        return 2 * Math.acos(Math.abs(Math.min(Math.max(this.dot(q), -1), 1)));
+    }
+
+    rotateTowards(q, step) {
+        const angle = this.angleTo(q);
+        if (angle === 0) return this;
+
+        const t = Math.min(1, step / angle);
+
+        this.slerp(q, t);
+        return this;
+    }
+
+    identity() {
+        return this.set( 0, 0, 0, 1 );
+    }
+
+    invert() {
+        // quaternion is assumed to have unit length
+        return this.conjugate();
+    }
+
+    conjugate() {
+        this._x *= -1;
+        this._y *= -1;
+        this._z *= -1;
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    dot(v) {
+        return this._x * v._x + this._y * v._y + this._z * v._z + this._w * v._w;
+    }
+
+    lengthSq() {
+        return this._x * this._x + this._y * this._y + this._z * this._z + this._w * this._w;
+    }
+
+    length() {
+        return Math.sqrt(this._x * this._x + this._y * this._y + this._z * this._z + this._w * this._w);
+    }
+
+    normalize() {
+        let l = this.length();
+
+        if (l === 0) {
+            this._x = 0;
+            this._y = 0;
+            this._z = 0;
+            this._w = 1;
+        } else {
+            l = 1 / l;
+            this._x = this._x * l;
+            this._y = this._y * l;
+            this._z = this._z * l;
+            this._w = this._w * l;
+        }
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    multiply(q) {
+        return this.multiplyQuaternions(this, q);
+    }
+
+    premultiply(q) {
+        return this.multiplyQuaternions(q, this);
+    }
+
+    multiplyQuaternions(a, b) {
+        // from http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/code/index.htm
+
+        const qax = a._x, qay = a._y, qaz = a._z, qaw = a._w;
+        const qbx = b._x, qby = b._y, qbz = b._z, qbw = b._w;
+
+        this._x = qax * qbw + qaw * qbx + qay * qbz - qaz * qby;
+        this._y = qay * qbw + qaw * qby + qaz * qbx - qax * qbz;
+        this._z = qaz * qbw + qaw * qbz + qax * qby - qay * qbx;
+        this._w = qaw * qbw - qax * qbx - qay * qby - qaz * qbz;
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    slerp(qb, t) {
+        if (t === 0) return this;
+        if (t === 1) return this.copy(qb);
+
+        const x = this._x, y = this._y, z = this._z, w = this._w;
+
+        // http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/slerp/
+
+        let cosHalfTheta = w * qb._w + x * qb._x + y * qb._y + z * qb._z;
+
+        if (cosHalfTheta < 0) {
+            this._w = -qb._w;
+            this._x = -qb._x;
+            this._y = -qb._y;
+            this._z = -qb._z;
+            cosHalfTheta = -cosHalfTheta;
+
+        } else {
+            this.copy(qb);
+        }
+
+        if (cosHalfTheta >= 1.0) {
+            this._w = w;
+            this._x = x;
+            this._y = y;
+            this._z = z;
+
+            return this;
+        }
+
+        const sqrSinHalfTheta = 1.0 - cosHalfTheta * cosHalfTheta;
+
+        if (sqrSinHalfTheta <= Number.EPSILON) {
+            const s = 1 - t;
+            this._w = s * w + t * this._w;
+            this._x = s * x + t * this._x;
+            this._y = s * y + t * this._y;
+            this._z = s * z + t * this._z;
+
+            this.normalize();
+            this._onChangeCallback();
+            return this;
+        }
+
+        const sinHalfTheta = Math.sqrt(sqrSinHalfTheta);
+        const halfTheta = Math.atan2(sinHalfTheta, cosHalfTheta);
+        const ratioA = Math.sin((1 - t) * halfTheta) / sinHalfTheta;
+        const ratioB = Math.sin(t * halfTheta) / sinHalfTheta;
+
+        this._w = (w * ratioA + this._w * ratioB);
+        this._x = (x * ratioA + this._x * ratioB);
+        this._y = (y * ratioA + this._y * ratioB);
+        this._z = (z * ratioA + this._z * ratioB);
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    slerpQuaternions(qa, qb, t) {
+        return this.copy(qa).slerp(qb, t);
+    }
+
+    random() {
+        // Derived from http://planning.cs.uiuc.edu/node198.html
+        // Note, this source uses w, x, y, z ordering, so we swap the order below.
+
+        const u1 = Math.random();
+        const sqrt1u1 = Math.sqrt(1 - u1);
+        const sqrtu1 = Math.sqrt(u1);
+        const u2 = 2 * Math.PI * Math.random();
+        const u3 = 2 * Math.PI * Math.random();
+
+        return this.set(
+            sqrt1u1 * Math.cos(u2),
+            sqrtu1 * Math.sin(u3),
+            sqrtu1 * Math.cos(u3),
+            sqrt1u1 * Math.sin(u2),
+        );
+    }
+
+    equals(quaternion) {
+        return (quaternion._x === this._x) && (quaternion._y === this._y) && (quaternion._z === this._z) && (quaternion._w === this._w);
+    }
+
+    fromArray(array, offset = 0) {
+        this._x = array[offset ];
+        this._y = array[offset + 1];
+        this._z = array[offset + 2];
+        this._w = array[offset + 3];
+
+        this._onChangeCallback();
+        return this;
+    }
+
+    toArray(array = [], offset = 0) {
+        array[ offset ] = this._x;
+        array[ offset + 1 ] = this._y;
+        array[ offset + 2 ] = this._z;
+        array[ offset + 3 ] = this._w;
+        return array;
+    }
+
+    fromBufferAttribute(attribute, index) {
+        this._x = attribute.getX(index);
+        this._y = attribute.getY(index);
+        this._z = attribute.getZ(index);
+        this._w = attribute.getW(index);
+        return this;
+    }
+
+    _onChange(callback) {
+        this._onChangeCallback = callback;
+        return this;
+    }
+
+    _onChangeCallback() {}
+
+    *[Symbol.iterator]() {
+        yield this._x;
+        yield this._y;
+        yield this._z;
+        yield this._w;
+    }
+
+}
+
+export { Quaternion };

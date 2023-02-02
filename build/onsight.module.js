0c931ddd804a534f451b131e922d6cb4c3c053f1
Stephens Nunnally <stevinz@gmail.com>
2023-02-02 00:24:50 -0800

    Updated math utils

--- build/onsight.module.js
+++ build/onsight.module.js
@@ -2,7 +2,7 @@
  * @description Onsight Engine
  * @about       Easy to use 2D / 3D JavaScript game engine.
  * @author      Stephens Nunnally <@stevinz>
- * @version     v0.0.3
+ * @version     v0.0.4
  * @license     MIT - Copyright (c) 2021-2023 Stephens Nunnally and Scidian Studios
  * @source      https://github.com/onsightengine
  */
@@ -19,7 +19,7 @@ import { Pass } from 'three/addons/postprocessing/Pass.js';
 import { LoopSubdivision } from 'three-subdivide';
 import { RoundedBoxGeometry } from 'three/addons/geometries/RoundedBoxGeometry.js';
 
-const VERSION = '0.0.3';
+const VERSION = '0.0.4';
 const BACKENDS = {
     RENDERER_3D: {
         PIXI:       'PIXI',
@@ -182,26 +182,13 @@ class CameraUtils {
     }
 }
 
+const _lut = [ '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '0a', '0b', '0c', '0d', '0e', '0f', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '1a', '1b', '1c', '1d', '1e', '1f', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '2a', '2b', '2c', '2d', '2e', '2f', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '3a', '3b', '3c', '3d', '3e', '3f', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '4a', '4b', '4c', '4d', '4e', '4f', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '5a', '5b', '5c', '5d', '5e', '5f', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '6a', '6b', '6c', '6d', '6e', '6f', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '7a', '7b', '7c', '7d', '7e', '7f', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '8a', '8b', '8c', '8d', '8e', '8f', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '9a', '9b', '9c', '9d', '9e', '9f', 'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'ba', 'bb', 'bc', 'bd', 'be', 'bf', 'c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'ca', 'cb', 'cc', 'cd', 'ce', 'cf', 'd0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'da', 'db', 'dc', 'dd', 'de', 'df', 'e0', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9', 'ea', 'eb', 'ec', 'ed', 'ee', 'ef', 'f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'fa', 'fb', 'fc', 'fd', 'fe', 'ff' ];
 class MathUtils {
     static radiansToDegrees(radians) {
         return radians * (180 / Math.PI);
     }
     static degreesToRadians(degrees) {
         return (Math.PI / 180) * degrees;
-    }
-    static fuzzyFloat(a, b, tolerance = 0.001) {
-        return ((a < (b + tolerance)) && (a > (b - tolerance)));
-    }
-    static fuzzyVector(a, b, tolerance = 0.001) {
-        if (MathUtils.fuzzyFloat(a.x, b.x, tolerance) === false) return false;
-        if (MathUtils.fuzzyFloat(a.y, b.y, tolerance) === false) return false;
-        if (MathUtils.fuzzyFloat(a.z, b.z, tolerance) === false) return false;
-        return true;
-    }
-    static fuzzyQuaternion(a, b, tolerance = 0.001) {
-        if (MathUtils.fuzzyVector(a, b, tolerance) === false) return false;
-        if (MathUtils.fuzzyFloat(a.w, b.w, tolerance) === false) return false;
-        return true;
     }
     static clamp(number, min, max) {
         number = Number(number);
@@ -218,6 +205,20 @@ class MathUtils {
     static roundTo(number, decimalPlaces = 0) {
         const shift = Math.pow(10, decimalPlaces);
         return Math.round(number * shift) / shift;
+    }
+    static fuzzyFloat(a, b, tolerance = 0.001) {
+        return ((a < (b + tolerance)) && (a > (b - tolerance)));
+    }
+    static fuzzyVector(a, b, tolerance = 0.001) {
+        if (MathUtils.fuzzyFloat(a.x, b.x, tolerance) === false) return false;
+        if (MathUtils.fuzzyFloat(a.y, b.y, tolerance) === false) return false;
+        if (MathUtils.fuzzyFloat(a.z, b.z, tolerance) === false) return false;
+        return true;
+    }
+    static fuzzyQuaternion(a, b, tolerance = 0.001) {
+        if (MathUtils.fuzzyVector(a, b, tolerance) === false) return false;
+        if (MathUtils.fuzzyFloat(a.w, b.w, tolerance) === false) return false;
+        return true;
     }
     static isPowerOfTwo(value) {
         return (value & (value - 1)) === 0 && value !== 0;
@@ -254,6 +255,18 @@ class MathUtils {
     }
     static randomInt(min = 0, max = 1) {
         return min + Math.floor(Math.random() * (max - min));
+    }
+    static uuid() {
+        if (window.crypto && window.crypto.randomUUID) return crypto.randomUUID();
+	    const d0 = Math.random() * 0xffffffff | 0;
+	    const d1 = Math.random() * 0xffffffff | 0;
+	    const d2 = Math.random() * 0xffffffff | 0;
+	    const d3 = Math.random() * 0xffffffff | 0;
+	    const uuid = _lut[ d0 & 0xff ] + _lut[ d0 >> 8 & 0xff ] + _lut[ d0 >> 16 & 0xff ] + _lut[ d0 >> 24 & 0xff ] + '-' +
+			_lut[ d1 & 0xff ] + _lut[ d1 >> 8 & 0xff ] + '-' + _lut[ d1 >> 16 & 0x0f | 0x40 ] + _lut[ d1 >> 24 & 0xff ] + '-' +
+			_lut[ d2 & 0x3f | 0x80 ] + _lut[ d2 >> 8 & 0xff ] + '-' + _lut[ d2 >> 16 & 0xff ] + _lut[ d2 >> 24 & 0xff ] +
+			_lut[ d3 & 0xff ] + _lut[ d3 >> 8 & 0xff ] + _lut[ d3 >> 16 & 0xff ] + _lut[ d3 >> 24 & 0xff ];
+	    return uuid.toLowerCase();
     }
 }
 
@@ -1392,7 +1405,7 @@ class World3D {
         this.isWorld3D = true;
         this.name = name;
         this.type = 'World3D';
-        this.uuid = crypto.randomUUID();
+        this.uuid = MathUtils.uuid();
         this.order = 0;
         this.startScene = null;
         this.lastEditorScene = null;
@@ -1445,7 +1458,7 @@ class Project {
     constructor(name = 'My Project') {
         this.isProject = true;
         this.name = name;
-        this.uuid = crypto.randomUUID();
+        this.uuid = MathUtils.uuid();
         this.type = 'Project';
         this.scripts = {};
         this.scenes = {};
@@ -1555,7 +1568,7 @@ class Project {
             this.removeScene(scene);
         }
         this.name = 'My Project';
-        this.uuid = crypto.randomUUID();
+        this.uuid = MathUtils.uuid();
     }
     fromJSON(json, loadAssets = true) {
         const metaType = (json.metadata) ? json.metadata.type : 'Undefined';
@@ -3005,19 +3018,19 @@ SkyObject.SkyShader = {
             float lowerGround =         0.0;
             float h = normalize(vWorldPosition + lowerGround).y;
 
-            ///// Sky fade (brighten at horizon)
+            // Sky fade (brighten at horizon)
             float skyFade = pow(vWorldPosition.y / (uScale * 0.25), 0.4);
             skyFade = clamp(skyFade, 0.0, 1.0);
             vec3 sky = mix(uHorizon, uSky, skyFade);
 
-            ///// Seperates ground and sky, solid horizon: clamp(h * uScale, 0.0, 1.0)
+            // Seperates ground and sky, solid horizon: clamp(h * uScale, 0.0, 1.0)
             float blurHorizon =         0.05;
             float compressHorizon =     5.0;
             float skyMix = max(pow(max(h, 0.0), blurHorizon) * compressHorizon, 0.0);
             skyMix = clamp(skyMix, 0.0, 1.0);
             vec3 outColor = mix(uGround, sky, skyMix);
 
-            ///// Output Color
+            // Output Color
             gl_FragColor = vec4(outColor, 1.0);
         }`
 };
@@ -4038,11 +4051,8 @@ Mesh.config = {
 ComponentManager.register('mesh', Mesh);
 
 if (typeof window !== 'undefined') {
-    if (window.__ONSIGHT__) {
-        console.warn('Multiple instances of Onsight being imported');
-    } else {
-        window.__ONSIGHT__ = VERSION;
-    }
+    if (window.__ONSIGHT__) console.warn(`Onsight v${window.__ONSIGHT__} already imported! Now importing v${VERSION}!`);
+    else window.__ONSIGHT__ = VERSION;
 }
 
 export { APP_STATES, App, AssetManager, BACKENDS, BasicLine, BasicWireBox, BasicWireframe, CAMERA_SCALE, CAMERA_START_DISTANCE, CAMERA_START_HEIGHT, CameraUtils, CapsuleGeometry, ComponentManager, CylinderGeometry, ENTITY_FLAGS, ENTITY_TYPES, Entity3D, EntityPool, EntityUtils, FatLine, FatWireBox, FatWireframe, GeometryUtils, GpuPickerPass, MathUtils, Object3D, ObjectUtils, PrismGeometry, Project, RenderUtils, SCENE_TYPES, SVGBuilder, Scene3D, Script, SkyObject, Strings, System, VERSION, Vectors, WORLD_TYPES, World3D };

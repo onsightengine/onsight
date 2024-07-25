7c4fcbbee1620eae84b4376dad5cc67169b219e6
Stephens Nunnally <stevinz@gmail.com>
2024-07-24 17:23:17 -0700

    Bump version

--- src/Onsight.js
+++ src/Onsight.js
@@ -1,9 +1,9 @@
 /**
- * @description Salinity
+ * @description Onsight
  * @about       Interactive, easy to use JavaScript game framework.
  * @author      Written by Stephens Nunnally <@stevinz>
  * @license     MIT - Copyright (c) 2024 Stephens Nunnally
- * @source      https://github.com/salinityengine
+ * @source      https://github.com/onsightengine
  */
 
 export * from './Core.js';
